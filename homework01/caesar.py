13776c088a0ee793564ce2c5bc3b07a38402f0dc
Pokhodnya <56508777+Pokhodnya@users.noreply.github.com>
2019-10-19 17:34:54 +0500

    реализованы функции

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -4,20 +4,16 @@ def encrypt_caesar(plaintext):
     'SBWKRQ'
     >>> encrypt_caesar("python")
     'sbwkrq'
-    >>> encrypt_caesar("Python3.6")
-    'Sbwkrq3.6'
     >>> encrypt_caesar("")
     ''
     """
-    ciphertext = ""
 
-    for i in range(len(plaintext)):
-        if (('X' <= plaintext[i] <= 'Z') or ('x' <= plaintext[i] <= 'z')):
-            ciphertext += chr(ord(plaintext[i]) - 23)
-        elif '!' <= plaintext[i] <= '@':
-            ciphertext += plaintext[i]
-        else:
-            ciphertext += chr(ord(plaintext[i]) + 3)
+    ciphertext = ''
+    for i in range(0, len(plaintext)):
+        if((65 <= ord(plaintext[i]) <= 87)or(97 <= ord(plaintext[i]) <= 119)):
+            ciphertext += chr(ord(a[i]) + 3)
+        elif((87 < ord(plaintext[i]) < 91)or(119 < ord(plaintext[i]) < 123)):
+            ciphertext += chr(ord(plaintext[i])-23)
 
     return ciphertext
 
@@ -28,19 +24,15 @@ def decrypt_caesar(ciphertext):
     'PYTHON'
     >>> decrypt_caesar("sbwkrq")
     'python'
-    >>> decrypt_caesar("Sbwkrq3.6")
-    'Python3.6'
     >>> decrypt_caesar("")
     ''
     """
-    plaintext = ""
 
-    for i in range(len(ciphertext)):
-        if ('A' <= ciphertext[i] <= 'C') or ('a' <= ciphertext[i] <= 'c'):
-            plaintext += chr(ord(ciphertext[i]) + 23)
-        elif '!' <= ciphertext[i] <= '@':
-            plaintext += ciphertext[i]
-        else:
-            plaintext += chr(ord(ciphertext[i]) - 3)
-
+    plaintext = ''
+    for i in range(0, len(ciphertext)):
+        if((68 <= ord(ciphertext[i]) <= 90) or (100 <= ord(a[i]) <= 122)):
+            plaintext += chr(ord(ciphertext[i])-3)
+        elif((64 < ord(ciphertext[i]) < 68)or(96 < ord(ciphertext[i]) <= 99)):
+            plaintext += chr(ord(ciphertext[i])+23)
     return plaintext
+
