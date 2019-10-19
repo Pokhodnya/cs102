0d31895c7a72faac20ccf7618ac2ee70c2e95598
Pokhodnya <56508777+Pokhodnya@users.noreply.github.com>
2019-10-19 17:49:07 +0500

    Update vigenere.py

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,4 +1,4 @@
-def encrypt_vigenere(plaintext: str, keyword: str) -> str:
+def encrypt_vigenere(plaintext, keyword):
     """
     >>> encrypt_vigenere("PYTHON", "A")
     'PYTHON'
@@ -7,11 +7,33 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
-    # PUT YOUR CODE HERE
+
+    a = 'abcdefghijklmnopqrstuvwxyz'
+    b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    ciphertext = ''
+    while len(plaintext) > len(keyword):
+        keyword += keyword
+    while len(keyword) > len(plaintext):
+        keyword = keyword[:-1]
+    for i in range(0, len(plaintext)):
+        if((plaintext[i] in a) or (plaintext[i] in b)):
+            if keyword[i] in a:
+                if ord(plaintext[i]) + a.index(keyword[i]) > 122:
+                    ciphertext += chr(ord(plaintext[i])+a.index(keyword[i])-26)
+                else:
+                    ciphertext += chr(ord(plaintext[i]) + a.index(keyword[i]))
+            if keyword[i] in b:
+                if ord(plaintext[i]) + b.index(d[i]) > 90:
+                    ciphertext += chr(ord(plaintext[i])+b.index(keyword[i])-26)
+                else:
+                    ciphertext += chr(ord(plaintext[i]) + b.index(keyword[i]))
+            else:
+                ciphertext += plaintext[i]
+
     return ciphertext
 
 
-def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
+def decrypt_vigenere(ciphertext, keyword):
     """
     >>> decrypt_vigenere("PYTHON", "A")
     'PYTHON'
@@ -20,5 +42,44 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
     'ATTACKATDAWN'
     """
-    # PUT YOUR CODE HERE
+
+    a = 'abcdefghijklmnopqrstuvwxyz'
+    b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    plaintext = ''
+    while len(ciphertext) > len(keyword):
+        keyword += keyword
+    while len(keyword) > len(ciphertext):
+        keyword = keyword[:-1]
+    for i in range(0, len(ciphertext)):
+        if((ciphertext[i] in a) or (ciphertext[i] in b)):
+            if keyword[i] in a:
+                if ord(ciphertext[i]) + a.index(keyword[i]) < 97:
+                    plaintext += chr(ord(ciphertext[i])-a.index(keyword[i])+26)
+                else:
+                    plaintext += chr(ord(ciphertext[i]) - a.index(keyword[i]))
+            if keyword[i] in b:
+                if ord(ciphertext[i]) + b.index(d[i]) < 65:
+                    plaintext += chr(ord(ciphertext[i])-b.index(keyword[i])+26)
+                else:
+                    plaintext += chr(ord(ciphertext[i]) - b.index(keyword[i]))
+            else:
+                plaintext += ciphertext[i]
+
+    return plaintext
+
+
+def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
+    plaintext = ''
+    while len(keyword) < len(ciphertext):
+        for i in keyword:
+            if len(keyword) < len(ciphertext):
+                keyword += i
+    if keyword.isupper():
+        code_A = 65
+    else:
+        code_A = 97
+    for ch, k in zip(list(ciphertext), list(keyword)):
+        if (ord(ch) < ord(k)):
+            ch = chr(ord(ch) + 26)
+        plaintext += chr(ord(ch) - (ord(k) - code_A))
     return plaintext
